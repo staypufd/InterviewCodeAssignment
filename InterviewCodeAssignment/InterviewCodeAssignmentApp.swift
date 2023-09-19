25b82b4319b0535bfde86f91b3cf84aa44307e0e
Sam Griffith <staypufd@mac.com>
2023-09-19 15:02:43 -0500

    Initial Commit

---
+++ InterviewCodeAssignment/InterviewCodeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewCodeAssignmentApp.swift
+//  InterviewCodeAssignment
+//
+//  Created by Sam Griffith on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewCodeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
